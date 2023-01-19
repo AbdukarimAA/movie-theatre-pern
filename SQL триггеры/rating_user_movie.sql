20f508a179c89d305667d4d9a73f8bca1f93cd89
Abdukarim Abdukhoshimov <93397997+AbdukarimAA@users.noreply.github.com>
2023-01-19 21:31:30 +0300

    Delete SQL триггеры directory

--- SQL триггеры/rating_user_movie.sql
+++
@@ -1,8 +0,0 @@
-CREATE FUNCTION CheckRating(integer, integer) RETURNS BOOLEAN AS $$
-BEGIN
-      RETURN EXISTS (SELECT * FROM ratings
-					JOIN users ON users.id = ratings."userId"
-					JOIN movies ON movies.id = ratings."movieId"
-					WHERE users.id = $1 AND movies.id = $2);
-END;
-$$ LANGUAGE plpgsql;
